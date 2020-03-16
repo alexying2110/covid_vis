2017d1b9b33742d3db4d70dcb9628c8fd9d3a539
alexying2110 <awy2110@columbia.edu>
2020-03-16 12:43:30 -0400

    logistic curves

--- app.r
+++ app.r
@@ -18,21 +18,31 @@ data = confirmed %>%
 today <- read.csv("./csse_data/csse_covid_19_data/csse_covid_19_daily_reports/03-15-2020.csv")
 
 ui <- fluidPage(
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput(
-        "Date",
-        "Dates:",
-        min = as.Date("2020-01-22","%Y-%m-%d"),
-        max = as.Date("2020-03-15","%Y-%m-%d"),
-        value = as.Date("2020-03-15","%Y-%m-%d"),
-        timeFormat="%m-%d-%Y",
-        animate = (animationOptions(interval = 500, loop = T))
-      )
-    ),
-    mainPanel(
-      leafletOutput(outputId = "mymap"), 
-    ),
+  fluidRow(
+    sidebarLayout(
+      sidebarPanel(
+        sliderInput(
+          "Date",
+          "Dates:",
+          min = as.Date("2020-01-22","%Y-%m-%d"),
+          max = as.Date("2020-03-15","%Y-%m-%d"),
+          value = as.Date("2020-03-15","%Y-%m-%d"),
+          timeFormat="%m-%d-%Y",
+          animate = (animationOptions(interval = 500, loop = T))
+        )
+      ),
+      mainPanel(
+        leafletOutput(outputId = "mymap"), 
+      ),
+    )
+  ),
+  fluidRow(
+    column(12, DT::dataTableOutput('tbl')),
+  ), 
+  fluidRow(
+    column(4, plotOutput('confirmed', height = 500)),
+    column(4, plotOutput('deaths', height = 500)),
+    column(4, plotOutput('recovered', height = 500))
   )
 )
 
@@ -73,6 +83,55 @@ server <- function(input, output, session) {
         color = ~pal(sqrt(eval(as.symbol(Deaths)) / eval(as.symbol(Confirmed)))),
         label = ~ifelse(Province.State == '', as.character(Country.Region), paste0(Province.State, ", ", Country.Region)),
     )
+  })
+  
+  output$tbl = DT::renderDataTable({
+    datatable(today[c(1:6)], selection = "single")
+    }, 
+    server = FALSE
+  )
+  
+  
+  output$confirmed = renderPlot({
+    selected <- input$tbl_rows_selected
+    if (length(selected) > 0) {
+      province <- as.character(today[selected,]$Province.State)
+      country <- as.character(today[selected,]$Country.Region)
+      selectedRow <- intersect(which(confirmed$Province.State == province), which(confirmed$Country.Region == country))
+      
+      dates <- gsub("X", "", names(confirmed)[-c(1:4)])
+      
+      confirmed_data <- data.frame(date = as.Date(dates, format="%m.%d.%y"), confirmed = as.numeric(confirmed[selectedRow,-c(1:4)]))
+      ggplot(data = confirmed_data, aes(x=date, y=confirmed)) + geom_line(color="orange") + geom_point(color="orange")
+    }
+  })
+  
+  output$deaths = renderPlot({
+    selected <- input$tbl_rows_selected
+    if (length(selected) > 0) {
+      province <- as.character(today[selected,]$Province.State)
+      country <- as.character(today[selected,]$Country.Region)
+      selectedRow <- intersect(which(deaths$Province.State == province), which(deaths$Country.Region == country))
+      
+      dates <- gsub("X", "", names(deaths)[-c(1:4)])
+      
+      deaths_data <- data.frame(date = as.Date(dates, format="%m.%d.%y"), deaths = as.numeric(deaths[selectedRow,-c(1:4)]))
+      ggplot(data = deaths_data, aes(x=date, y=deaths)) + geom_line(color="red") + geom_point(color="red")
+    }
+  })
+  
+  output$recovered = renderPlot({
+    selected <- input$tbl_rows_selected
+    if (length(selected) > 0) {
+      province <- as.character(today[selected,]$Province.State)
+      country <- as.character(today[selected,]$Country.Region)
+      selectedRow <- intersect(which(recovered$Province.State == province), which(recovered$Country.Region == country))
+      
+      dates <- gsub("X", "", names(recovered)[-c(1:4)])
+      
+      recovered_data <- data.frame(date = as.Date(dates, format="%m.%d.%y"), recovered = as.numeric(recovered[selectedRow,-c(1:4)]))
+      ggplot(data=recovered_data, aes(x=date, y=recovered)) + geom_line(color="green") + geom_point(color="green")
+    }
   })
 }
 
