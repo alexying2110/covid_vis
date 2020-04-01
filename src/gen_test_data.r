d820d846adaead6615bdd7ef1abe3fdcbcc6f6ec
alexying2110 <awy2110@columbia.edu>
2020-04-01 00:47:19 -0400

    added fields to test data generator

--- src/gen_test_data.r
+++ src/gen_test_data.r
@@ -5,13 +5,18 @@ setwd("/home/lofatdairy/code/sialab/covid_vis")
 startData <- fread("csse_data/csse_covid_19_data/csse_covid_19_daily_reports/03-24-2020.csv")
 startData <- startData[Country_Region == "US"]
 
-currentTime <- 1585363185178
+currentTime <- as.numeric(as.POSIXct(Sys.time()))
 startTime <- currentTime - 1000 * 60 * 60 * 24 * 5
+
+nObs <- 100000
   
-testObs <- sample_n(startData, size = 100000, replace = T, weight = Confirmed)
+testObs <- sample_n(startData, size = nObs, replace = T, weight = Confirmed)
 testObs <- testObs[, c("Admin2", "Province_State", "Lat", "Long_")]
 names(testObs) <- c("County", "State", "Lat", "Long")
-testObs$Positive <- runif(100000) < .4
-testObs$Updated <- sample(startTime:currentTime, 100000, replace = T)
+testObs$Positive <- runif(nObs) < .4
+testObs$Updated <- sample(startTime:currentTime, nObs, replace = T)
+testObs$Race <- ifelse(runif(nObs) < .75, "White", ifelse(runif(200) < .6, "Black", "Asian"))
+testObs$Hispanic <- runif(nObs) < .12
+testObs$Age <- floor(rnorm(nObs, 48, 12))
 
 fwrite(testObs, file = "./our_data/test/test.csv")
