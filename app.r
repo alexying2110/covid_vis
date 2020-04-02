58f4ee94c5f138048877060fa03dc9bd65a46e5b
alexying2110 <awy2110@columbia.edu>
2020-04-02 18:21:34 -0400

    added census estimate data for 2019

--- app.r
+++ app.r
@@ -10,7 +10,8 @@ library(ggplot2)
 library(rsconnect)
 library(profvis)
 
-setwd("/home/ubuntu/covid_vis")
+#setwd("/home/ubuntu/covid_vis")
+setwd("/home/lofatdairy/code/sialab/covid_vis")
 
 counties <- readOGR("our_data/US/counties.json")
 #data <- fread("csse_data/csse_covid_19_data/csse_covid_19_daily_reports/03-24-2020.csv")
@@ -24,6 +25,7 @@ ui <- fluidPage(
   )
 )
 
+pal <- colorBin(colorRamp(c("#FFDD00","#FF0000")), domain = NULL, bins = 10)
 server <- function(input, output, session) {
   obs <- fread("our_data/test/test.csv")
   aggregated <- obs[, .(Tests = length(Positive), Positive = sum(Positive)), by = .(County, State)]
@@ -34,17 +36,14 @@ server <- function(input, output, session) {
   counties$cases <- aggregated[if_else(counties$STATENAME == "New York" & counties$NAME %in% c("Bronx", "New York", "Kings", "Queens", "Richmond"), "New York City, New York", paste0(counties$NAME, ", ", counties$STATENAME)), Positive]
   counties$cases[is.na(counties$cases)] <- 0
   
-  pal <- colorBin(colorRamp(c("#FFDD00","#FF0000")), domain = NULL, bins = 15)
   output$mymap <- renderLeaflet({
     leaflet(counties) %>%
       addTiles() %>%
       addProviderTiles(providers$CartoDB.DarkMatterNoLabels) %>% 
-      addPolygons(stroke = FALSE, smoothFactor = 0.3, fillOpacity = 1,
-                  label = ~paste0(NAME),
-                  group = "Tested"
-                ) %>%
-      addPolygons(stroke = FALSE, smoothFactor = 0.3, fillOpacity = ~ifelse(cases == 0, .5, .7),
-                  color = ~ifelse(cases == 0, "#00FF00", pal(as.numeric(log10(cases)))),
+      addPolygons(stroke = FALSE, 
+                  smoothFactor = 0.3,
+                  fillOpacity = ~ifelse(cases == 0, .5, .7),
+                  color = ~if_else(cases == 0, "#00FF00", pal(as.numeric(log10(cases)))),
                   label = ~paste0(NAME, ", ", STATENAME, ": ", cases),
                   group = "Positive"
                 ) %>%
