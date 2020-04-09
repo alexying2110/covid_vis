0d2b84a6b835c90ba563b5b8a611e98bbea991f5
alexying2110 <awy2110@columbia.edu>
2020-04-08 20:01:02 -0400

    added county graphs and time slider

--- app.r
+++ app.r
@@ -33,8 +33,29 @@ pal <- colorBin(colorRamp(c("#FFDD00","#FF0000")), domain = NULL, bins = 10)
 # Note here that any additional graphs, buttons, etc must be added to ui, which just holds a fluid page object
 ui <- fluidPage(
   fluidRow(
+    sidebarPanel(
+      sliderInput("time", 
+                  label = h3("Time"), 
+                  min = 0, 
+                  max = 0, 
+                  value = 0, 
+                  animate = T,
+                  ticks = T
+                  ),
+    ),
     mainPanel(
       leafletOutput(outputId = "map")
+    )
+  ),
+  fluidRow(
+    column(4,
+      plotOutput("logisticCurve")
+    ),
+    column(4,
+      plotOutput("posAge")
+    ),
+    column(4,
+      plotOutput("posRace")
     )
   )
 )
@@ -48,97 +69,115 @@ server <- function(input, output, session) {
   # observation being a single test taken. each observation is the county, state, lat, long, outcome (positive or negative)
   # update time, race, hispanic, and age of the user/test.
   obs <- fread("our_data/test/test.csv")
+  updateSliderInput(
+    session, 
+    "time", 
+    value = as.POSIXct(max(obs$Updated), origin = "1970-01-01"), 
+    min   = as.POSIXct(min(obs$Updated), origin = "1970-01-01"),
+    max   = as.POSIXct(max(obs$Updated), origin = "1970-01-01"),
+    timeFormat = "%b %d %Y, %H:%M"
+    )
   
-  pop <- fread("our_data/US/census_pop_2019.csv")
-  pop$CTYNAME[1835] <- "Dona Ana County"
-  pop[, Location := paste0(unlist(strsplit(CTYNAME, " County")), ", ", STNAME)]
-  setkey(pop, Location)
-  
-  # aggregated is basically taking all the observations in obs, and grouping them by state and county (some county
-  # names are the same in different states, so we use both). For example, if there are 100 observations in LA, California,
-  # and 50 are positive, then the aggregated table will look like this:
-  # County, State, Tests, Positive, Location
-  # Los Angeles, California, 100, 50, "Los Angeles, California"
-  # Note that Location is basically the combination of county and state name (again because county name is not unique)
-  # to generate a unique string, and then location is made the key for the data.table, basically turning it into a hash
-  # map. This is important later, because we have to map number of tests and number of cases to each county for the 
-  # counties dictionary, and this lets us accomplish that quickly and concisely
-  aggregated <- obs[, .(Tests = length(Positive), Positive = sum(Positive)), by = .(County, State)]
-  aggregated[, Location := paste0(County, ", ", State)]
-  setkey(aggregated, Location)
-  
-  # Remove if_else if counties are actually reported correctly.
-  # This line is somewhat busy, and there's a few things going on. The general idea is this: we need a vector containing
-  # the number of positive cases in each county, and the counts NEED to be in the same order as the order of the county
-  # names in counties$NAMES, otherwise the counts won't be correct. The way this is done is simply taking the counties$NAME
-  # and counties$STATENAME vector, and combining them item by item to form "NAME, STATENAME" strings, which match the format
-  # of our keys in aggregated. Then we can go along this new vector, and treat it as a key vector for our aggregated hash table,
-  # which ends up returning a data.table that contains only the rows we queried for, in the same order we queried them in. We then
-  # only care about the "Positive" field, because we're getting number of cases, so we extract that column as a vector at the end.
-  # The reason that there's a somewhat convoluted in_else function in there, is because NY state is aggrevating and reports all
-  # cases in all 5 boroughs as "NYC", so queries like "Bronx, New York", don't have an entry in aggregated, and would be NA. So
-  # we just treat all these locations as "New York City, New York", and return the number of cases there.
-  counties$cases <- aggregated[if_else(counties$STATENAME == "New York" & counties$NAME %in% c("Bronx", "New York", "Kings", "Queens", "Richmond"), "New York City, New York", paste0(counties$NAME, ", ", counties$STATENAME)), Positive]
-  
-  # This line basically takes all NA values in the counties$cases vector and converts them to 0. Counties will no cases are not
-  # listed in aggregated
-  counties$cases[is.na(counties$cases)] <- 0
-  
-  # Basically the same thing as the previous line except now we match counties to the number of tests
-  counties$tests <- aggregated[if_else(counties$STATENAME == "New York" & counties$NAME %in% c("Bronx", "New York", "Kings", "Queens", "Richmond"), "New York City, New York", paste0(counties$NAME, ", ", counties$STATENAME)), Tests]
-  counties$tests[is.na(counties$tests)] <- 0
-  
-  # Basically the same thing as the previous line except now we match counties to the number of tests
-  # counties$pop <- pop[paste0(counties$NAME, ", ", counties$STATENAME), POPESTIMATE2019]
-  
-  # To the output object, we add the map key, and assign it a leaflet map.
-  output$map <- renderLeaflet({
-    #the leaflet constructor is given the geojson list
-    leaflet(counties) %>%
-      addTiles() %>%
-      addProviderTiles(providers$CartoDB.DarkMatterNoLabels) %>% 
-      # This generates polygons that go over the map, and color them based on the log of the number of cases. Note you will see an warning
-      # that goes something like "pal got some values outside of it's range so it'll treat them as NA", but you can ignore this, because
-      # it's referring to log10(0), for counties with no cases, and this exception is being handled by the outer if_else.
-      addPolygons(stroke = FALSE, 
-                  smoothFactor = 0.3,
-                  fillOpacity = ~ifelse(cases == 0, .5, .7),
-                  layerId = ~paste0(NAME, ",", STATENAME),
-                  color = ~if_else(cases == 0, "#00FF00", pal(as.numeric(log10(cases)))),
-                  label = ~paste0(NAME, ", ", STATENAME, ": ", cases),
-                  group = "Positive"
-                ) %>%
-      # This adds a simple selector for which polygons you want to see, based on the group they belong to.
-      addLayersControl(
-        baseGroups = c("Positive", "Tested"),
-        options = layersControlOptions(collapsed = FALSE)
-      )
+  observeEvent(input$time, {
+    # TODO: handle the fact that county names are fucked, and that state names are reproduced
+    # pop <- fread("our_data/US/census_pop_2019.csv")
+    # pop$CTYNAME[1835] <- "Dona Ana County"
+    # pop <- pop[!(CTYNAME == "District of Columbia" & COUNTY == 1)]
+    # pop[Location := paste0(unlist(strsplit(CTYNAME, " County")), ", ", STNAME)]
+    # setkey(pop, Location)
+    
+    # aggregated is basically taking all the observations in obs, and grouping them by state and county (some county
+    # names are the same in different states, so we use both). For example, if there are 100 observations in LA, California,
+    # and 50 are positive, then the aggregated table will look like this:
+    # County, State, Tests, Positive, Location
+    # Los Angeles, California, 100, 50, "Los Angeles, California"
+    # Note that Location is basically the combination of county and state name (again because county name is not unique)
+    # to generate a unique string, and then location is made the key for the data.table, basically turning it into a hash
+    # map. This is important later, because we have to map number of tests and number of cases to each county for the 
+    # counties dictionary, and this lets us accomplish that quickly and concisely
+    aggregated <- obs[, .(Tests = length(Positive), Positive = sum(Positive)), by = .(County, State)]
+    aggregated[, Location := paste0(County, ", ", State)]
+    setkey(aggregated, Location)
+    
+    # Remove if_else if counties are actually reported correctly.
+    # This line is somewhat busy, and there's a few things going on. The general idea is this: we need a vector containing
+    # the number of positive cases in each county, and the counts NEED to be in the same order as the order of the county
+    # names in counties$NAMES, otherwise the counts won't be correct. The way this is done is simply taking the counties$NAME
+    # and counties$STATENAME vector, and combining them item by item to form "NAME, STATENAME" strings, which match the format
+    # of our keys in aggregated. Then we can go along this new vector, and treat it as a key vector for our aggregated hash table,
+    # which ends up returning a data.table that contains only the rows we queried for, in the same order we queried them in. We then
+    # only care about the "Positive" field, because we're getting number of cases, so we extract that column as a vector at the end.
+    # The reason that there's a somewhat convoluted in_else function in there, is because NY state is aggrevating and reports all
+    # cases in all 5 boroughs as "NYC", so queries like "Bronx, New York", don't have an entry in aggregated, and would be NA. So
+    # we just treat all these locations as "New York City, New York", and return the number of cases there.
+    counties$cases <- aggregated[if_else(counties$STATENAME == "New York" & counties$NAME %in% c("Bronx", "New York", "Kings", "Queens", "Richmond"), "New York City, New York", paste0(counties$NAME, ", ", counties$STATENAME)), Positive]
+    
+    # This line basically takes all NA values in the counties$cases vector and converts them to 0. Counties will no cases are not
+    # listed in aggregated
+    counties$cases[is.na(counties$cases)] <- 0
+    
+    caseTest <- counties$cases
+    
+    # Basically the same thing as the previous line except now we match counties to the number of tests
+    counties$tests <- aggregated[if_else(counties$STATENAME == "New York" & counties$NAME %in% c("Bronx", "New York", "Kings", "Queens", "Richmond"), "New York City, New York", paste0(counties$NAME, ", ", counties$STATENAME)), Tests]
+    counties$tests[is.na(counties$tests)] <- 0
+    
+    # Basically the same thing as the previous line except now we match counties to the number of tests
+    # counties$pop <- pop[paste0(counties$NAME, ", ", counties$STATENAME), POPESTIMATE2019]
+    
+    # To the output object, we add the map key, and assign it a leaflet map.
+    output$map <- renderLeaflet({
+      #the leaflet constructor is given the geojson list
+      leaflet(counties) %>%
+        addTiles() %>%
+        addProviderTiles(providers$CartoDB.DarkMatterNoLabels) %>% 
+        # This generates polygons that go over the map, and color them based on the log of the number of cases. Note you will see an warning
+        # that goes something like "pal got some values outside of it's range so it'll treat them as NA", but you can ignore this, because
+        # it's referring to log10(0), for counties with no cases, and this exception is being handled by the outer if_else.
+        addPolygons(stroke = FALSE, 
+                    smoothFactor = 0.3,
+                    fillOpacity = ~ifelse(cases == 0, .5, .7),
+                    layerId = ~paste0(NAME, ",", STATENAME),
+                    color = ~if_else(cases == 0, "#00FF00", pal(as.numeric(log10(caseTest)))),
+                    label = ~paste0(NAME, ", ", STATENAME, ": ", cases),
+                    group = "Positive"
+                  ) %>%
+        # This adds a simple selector for which polygons you want to see, based on the group they belong to.
+        addLayersControl(
+          baseGroups = c("Positive", "Tested"),
+          options = layersControlOptions(collapsed = FALSE)
+        )
+    })
   })
   
   # you can ignore these, i'm still working on this.
-  output$logistic_curve <- renderPlot({
-    id <- strsplit(input$map_shape_click, ",")
-    subset <- obs[County == id[1] & State == id[2] & Positive]
-    setorder(subset, Updated)
-    subset[, nCases := as.numeric(row.names(subset))]
-    ggplot(subset, aes(x = Updated, y = nCases)) + 
-      geom_step() + 
-      scale_y_continuous(name = "Number of Cases") +
-      scale_x_continuous(name = "Date", labels = function(x) {as.Date(as.POSIXct(x, origin = "1970-01-01"))})
-  })
-  
-  output$posRace <- renderPlot({
-    id <- strsplit(input$map_shape_click, ",")[[1]]
-    subset <- obs[County == id[1] & State == id[2] & Positive]
-    breakdown <- subset[, length(Positive)/nrow(subset) * 100, by = Race]
-    ggplot(breakdown, aes(y = V1, fill = Race)) + geom_bar(width = 1) + coord_polar("y", start = 0)
-  })
-  
-  output$posAge <- renderPlot({
-    id <- strsplit(input$map_shape_click, ",")[[1]]
-    subset <- obs[County == id[1] & State == id[2] & Positive]
-    breakdown <- subset[]
-    ggplot(subset, aes(y = Race))
+  observeEvent(input$map_shape_click, {
+    id <- strsplit(input$map_shape_click$id, ",")[[1]]
+    print(id)
+    output$logisticCurve <- renderPlot({
+      subset <- obs[County == id[1] & State == id[2] & Positive]
+      setorder(subset, Updated)
+      subset[, nCases := as.numeric(row.names(subset))]
+      ggplot(subset, aes(x = Updated, y = nCases)) + 
+        geom_step() + 
+        theme_minimal() +
+        scale_y_continuous(name = "Number of Cases") +
+        scale_x_continuous(name = "Date", labels = function(x) {as.Date(as.POSIXct(x, origin = "1970-01-01"))})
+    })
+    
+    output$posRace <- renderPlot({
+      subset <- obs[County == id[1] & State == id[2] & Positive]
+      ggplot(subset, aes(Race)) + 
+        geom_bar() + 
+        theme_minimal()
+    })
+
+    output$posAge <- renderPlot({
+      subset <- obs[County == id[1] & State == id[2] & Positive]
+      ggplot(subset, aes(Age)) +
+        geom_histogram(bins = 5) +
+        theme_minimal()
+    })
   })
 }
 
