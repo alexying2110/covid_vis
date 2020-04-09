8b73c38c89c3317cee6b64bd49a54d14a4bf3742
alexying2110 <awy2110@columbia.edu>
2020-04-09 10:43:34 -0400

    switching polygons to circle markers

--- src/gen_test_data.r
+++ src/gen_test_data.r
@@ -6,6 +6,12 @@ setwd("/home/lofatdairy/code/sialab/covid_vis")
 startData <- fread("csse_data/csse_covid_19_data/csse_covid_19_daily_reports/03-24-2020.csv")
 startData <- startData[Country_Region == "US"]
 
+
+countyCenters <- startData[, c("Admin2", "Province_State", "Lat", "Long_")]
+names(countyCenters) <- c("County", "State", "Lat", "Long")
+countyCenters[, Location := paste0(County, ", ", State)]
+fwrite(countyCenters, "./our_data/US/county_centers.csv")
+
 currentTime <- floor(as.numeric(as.POSIXct(Sys.time())))
 startTime <- currentTime - 60 * 60 * 24
 
@@ -14,6 +20,7 @@ nObs <- 10000
 testObs <- sample_n(startData, size = nObs, replace = T, weight = Confirmed)
 testObs <- testObs[, c("Admin2", "Province_State", "Lat", "Long_")]
 names(testObs) <- c("County", "State", "Lat", "Long")
+
 testObs$Positive <- runif(nObs) < .4
 testObs$Updated <- sample(startTime:currentTime, nObs, replace = T)
 testObs$Race <- ifelse(runif(nObs) < .75, "White", ifelse(runif(200) < .6, "Black", "Asian"))
