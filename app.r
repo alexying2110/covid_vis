27327efbbf036ba54c68f5465f54963963223ce6
alexying2110 <awy2110@columbia.edu>
2020-04-17 23:12:52 -0400

    controls working now, todos in comments

--- app.r
+++ app.r
@@ -13,7 +13,7 @@ library(scales)
 library(shinydashboard)
 
 # setwd("/home/ubuntu/covid_vis")
-# setwd("/home/lofatdairy/code/sialab/covid_vis")
+setwd("/home/lofatdairy/code/sialab/covid_vis")
 
 sidebar <- dashboardSidebar(
   sidebarMenu(
@@ -36,9 +36,12 @@ body <- dashboardBody(
                           min = 0, 
                           max = 0, 
                           value = 0, 
-                          animate = T,
+                          step = 300000,
+                          animate = animationOptions(interval = 100, loop = T),
                           ticks = T
-                )
+                ),
+                radioButtons("markers", label = h3("Placeholder"), choices = c("Cases", "Tests", "Cases Per Capita")),
+                radioButtons("counties", label = h3("Placeholder 2"), choices = c("Population", "Beds", "Elderly Population", "Comorbidities"))
               ),
               mainPanel(
                 leafletOutput(outputId = "map")
@@ -79,18 +82,35 @@ ui <- dashboardPage(
 pal <- colorBin(colorRamp(c("#FFDD00","#FF0000")), domain = NULL, bins = 10)
 
 counties <- readOGR("our_data/US/counties.json")
-countyCenters <- fread("our_data/US/county_centers.csv")
+countyCenters <- fread("our_data/US/county_centers.csv", key = "Location")
+
+beds <- fread("our_data/US/hospitals.csv")
+beds <- beds[, .(Beds = sum(BEDS)), by = .(COUNTY, STATE)]
+beds[, Location := paste0(COUNTY, ", ", STATE)]
+setkey(beds, Location)
+
+#have to clean ths fucking beds dt jfc
 
 server <- function(input, output, session) {
   obs <- fread("our_data/test/test.csv")
+  
+  # Round to nearest 5 minute
+  updateMax <- ceiling(max(obs$Updated) / 300) * 300
+  updateMin <- floor(min(obs$Updated) / 300) * 300
   updateSliderInput(
     session, 
     "time", 
-    value = as.POSIXct(max(obs$Updated), origin = "1970-01-01"), 
-    min   = as.POSIXct(min(obs$Updated), origin = "1970-01-01"),
-    max   = as.POSIXct(max(obs$Updated), origin = "1970-01-01"),
+    value = as.POSIXct(updateMax, origin = "1970-01-01"), 
+    min   = as.POSIXct(updateMin, origin = "1970-01-01"),
+    max   = as.POSIXct(updateMax, origin = "1970-01-01"),
     timeFormat = "%b %d %Y, %H:%M"
   )
+  
+  counties$beds <- beds[paste0(counties$NAME, ", ", counties$STATENAME), Beds]
+  counties$beds[is.na(counties$beds)] <- 0 
+  
+  # counties$ages <- aggregated[paste0(counties$NAME, ", ", counties$STATENAME), Ages]
+  # counties$ages[is.na(counties$ages)] <- 0
   
   # TODO: handle the fact that county names are fucked, and that state names are reproduced
   # pop <- fread("our_data/US/census_pop_2019.csv")
@@ -99,128 +119,106 @@ server <- function(input, output, session) {
   # pop[Location := paste0(unlist(strsplit(CTYNAME, " County")), ", ", STNAME)]
   # setkey(pop, Location)
   
-  aggregated <- obs[, .(Tests = length(Positive), Positive = sum(Positive)), by = .(County, State)]
-  aggregated[, Location := paste0(County, ", ", State)]
-  setkey(aggregated, Location)
-  
-  cases <- aggregated[if_else(counties$STATENAME == "New York" & counties$NAME %in% c("Bronx", "New York", "Kings", "Queens", "Richmond"), "New York City, New York", paste0(counties$NAME, ", ", counties$STATENAME)), Positive]
-  cases[is.na(counties$cases)] <- 0
-  casesReact <- reactiveVal(cases)
-  
-    #pop <- fread("our_data/US/census_pop_2019.csv")
-    #pop$CTYNAME[1835] <- "Dona Ana County"
-    #pop[, Location := paste0(unlist(strsplit(CTYNAME, " County")), ", ", STNAME)]
-    #setkey(pop, Location)
-    # Basically the same thing as the previous line except now we match counties to the number of tests
-    #counties$pop <- pop[paste0(counties$NAME, ", ", counties$STATENAME), POPESTIMATE2019]
-    
-    
-    # counties$pop <- pop[paste0(counties$NAME, ", ", counties$STATENAME), POPESTIMATE2019]
-  tests <- aggregated[if_else(counties$STATENAME == "New York" & counties$NAME %in% c("Bronx", "New York", "Kings", "Queens", "Richmond"), "New York City, New York", paste0(counties$NAME, ", ", counties$STATENAME)), Tests]
-  tests[is.na(counties$tests)] <- 0
-  testsReact <- reactiveVal(tests)
-  beds <- fread("our_data/US/hospitals.csv")
-  beds <- beds[, .(Beds = sum(BEDS)), by = .(COUNTY, STATE)]
-  beds[, Location := paste0(COUNTY, ", ", STATE)]
-  setkey(beds, Location)
-
-  counties$beds <- beds[if_else(counties$STATENAME == "New York" & counties$NAME %in% c("Bronx", "New York", "Kings", "Queens", "Richmond"), "New York City, New York", paste0(counties$NAME, ", ", counties$STATENAME)), Beds]
-  counties$beds[is.na(counties$beds)] <- 0 
-  
-  counties$cases <- aggregated[if_else(counties$STATENAME == "New York" & counties$NAME %in% c("Bronx", "New York", "Kings", "Queens", "Richmond"), "New York City, New York", paste0(counties$NAME, ", ", counties$STATENAME)), Positive]
-  counties$cases[is.na(counties$cases)] <- 0
-
-  counties$tests <- aggregated[if_else(counties$STATENAME == "New York" & counties$NAME %in% c("Bronx", "New York", "Kings", "Queens", "Richmond"), "New York City, New York", paste0(counties$NAME, ", ", counties$STATENAME)), Tests]
-  counties$tests[is.na(counties$tests)] <- 0
-  
-  # counties$ages <- aggregated[if_else(counties$STATENAME == "New York" & counties$NAME %in% c("Bronx", "New York", "Kings", "Queens", "Richmond"), "New York City, New York", paste0(counties$NAME, ", ", counties$STATENAME)), Ages]
-  # counties$ages[is.na(counties$ages)] <- 0
-
-  observeEvent(input$time, {
-    unixTime <- as.numeric(input$time)
-    
-    output$mymap <- renderLeaflet({
-      leaflet(counties) %>%
-        addTiles() %>%
-        addProviderTiles(providers$CartoDB.DarkMatterNoLabels) %>% 
-        addPolygons(stroke = FALSE, 
-                    smoothFactor = 0.3,
-                    fillOpacity = ~ifelse(cases == 0, .5, .7),
-                    color = ~if_else(cases == 0, "#00FF00", pal(as.numeric(log10(tests)))),
-                    label = ~paste0(NAME, ", ", STATENAME, ": ", cases),
-                    group = "Positive"
-        )%>%
-        addPolygons(stroke = FALSE, 
-                    smoothFactor = 0.3,
-                    fillOpacity = ~ifelse(tests == 0, .5, .7),
-                    color = ~if_else(tests == 0, "#00FF00", pal(as.numeric(log10(tests)))),
-                    label = ~paste0(NAME, ", ", STATENAME, ": ", tests),
-                    group = "Tested"
-        ) %>%
-        # addPolygons(stroke = FALSE, 
-        #             smoothFactor = 0.3,
-        #             fillOpacity = ~ifelse(ages == 0, .5, .7),
-        #             color = ~if_else(ages == 0, "#00FF00", pal(as.numeric(log10(ages)))),
-        #             label = ~paste0(NAME, ", ", STATENAME, ": ", ages),
-        #             group = "Ages"
-        # ) %>%
-        addPolygons(stroke = FALSE, 
-                    smoothFactor = 0.3,
-                    fillOpacity = ~ifelse(beds == 0, .5, .7),
-                    color = ~if_else(beds == 0, "#00FF00", pal(as.numeric(log10(beds)))),
-                    label = ~paste0(NAME, ", ", STATENAME, ": ", beds),
-                    group = "Hospital Beds"
-        ) %>%
-        addLayersControl(
-          baseGroups = c("Positive", "Tested", "Ages", "Hospital Beds"),
-          options = layersControlOptions(collapsed = FALSE)
-        )
-    })
-  }) #closing bracket for observeEvent()
+  listener <- reactive({
+    list(input$markers, input$time)
+  })
   
   output$map <- renderLeaflet({
     leaflet(counties) %>%
       addProviderTiles(providers$CartoDB.DarkMatterNoLabels) %>%
-      addPolygons(stroke = FALSE, 
+      setView(lng = -97, lat = 39, zoom = 3) %>%
+      addPolygons(stroke = FALSE,
                   smoothFactor = 0.3,
-                  fillOpacity = ~ifelse(cases == 0, .5, .7),
-                  color = ~if_else(cases == 0, "#00FF00", pal(as.numeric(log10(tests)))),
-                  label = ~paste0(NAME, ", ", STATENAME, ": ", cases),
-                  group = "Positive"
-      )%>%
-      addPolygons(stroke = FALSE, 
-                  smoothFactor = 0.3,
-                  fillOpacity = ~ifelse(tests == 0, .5, .7),
-                  color = ~if_else(tests == 0, "#00FF00", pal(as.numeric(log10(tests)))),
-                  label = ~paste0(NAME, ", ", STATENAME, ": ", tests),
-                  group = "Tested"
+                  fillOpacity = ~ifelse(ages == 0, .5, .7),
+                  color = ~pal(as.numeric(beds)),
+                  label = ~paste0(NAME, ", ", STATENAME, ": ", ages),
+                  group = "population"
       ) %>%
-      # addPolygons(stroke = FALSE, 
-      #             smoothFactor = 0.3,
-      #             fillOpacity = ~ifelse(ages == 0, .5, .7),
-      #             color = ~if_else(ages == 0, "#00FF00", pal(as.numeric(log10(ages)))),
-      #             label = ~paste0(NAME, ", ", STATENAME, ": ", ages),
-      #             group = "Ages"
-      # ) %>%
-      addPolygons(stroke = FALSE, 
+      addPolygons(stroke = FALSE,
                   smoothFactor = 0.3,
-                  fillOpacity = ~ifelse(beds == 0, .5, .7),
-                  color = ~if_else(beds == 0, "#00FF00", pal(as.numeric(log10(beds)))),
-                  label = ~paste0(NAME, ", ", STATENAME, ": ", beds),
-                  group = "Hospital Beds"
+                  fillOpacity = ~ifelse(ages == 0, .5, .7),
+                  color = ~pal(as.numeric(beds)),
+                  label = ~paste0(NAME, ", ", STATENAME, ": ", ages),
+                  group = "beds"
       ) %>%
-      addLayersControl(
-        baseGroups = c("Positive", "Tested", "Ages", "Hospital Beds"),
-        options = layersControlOptions(collapsed = FALSE)
+      addPolygons(stroke = FALSE,
+                  smoothFactor = 0.3,
+                  fillOpacity = ~ifelse(ages == 0, .5, .7),
+                  color = ~pal(as.numeric(beds)),
+                  label = ~paste0(NAME, ", ", STATENAME, ": ", ages),
+                  group = "elderly"
+      ) %>%
+      addPolygons(stroke = FALSE,
+                  smoothFactor = 0.3,
+                  fillOpacity = ~ifelse(ages == 0, .5, .7),
+                  color = ~pal(as.numeric(beds)),
+                  label = ~paste0(NAME, ", ", STATENAME, ": ", ages),
+                  group = "comorbidity"
       )
   })
   
-  observeEvent(input$time, {
-    time <- as.numeric(input$time)
+  observeEvent(listener(), {
+    unixTime <- as.numeric(input$time)
+    aggregated <- obs[Updated < unixTime, .(Tests = length(Positive), Positive = sum(Positive)), by = .(County, State)]
+    aggregated[, Location := paste0(County, ", ", State)]
+    setkey(aggregated, Location)
+    
+    if (input$markers == "Tests") {
+      aggregated[, Markers := Tests]
+    }
+    
+    if (input$markers == "Cases") {
+      aggregated[, Markers := Positive]
+    }
+    
+    if (input$markers == "Cases per Capita") {
+      aggregated[, Markers := Tests]
+    }
+    
+    aggregated$Lat <- countyCenters[aggregated$Location, Lat]
+    aggregated$Long <- countyCenters[aggregated$Location, Long]
+    
+    leafletProxy("map", data = aggregated) %>%
+      clearGroup(group = "marker") %>%
+      addCircleMarkers(lng = ~Long, 
+                 lat = ~Lat, layerId = ~Location,
+                 radius = ~log10(Markers) * 5,
+                 opacity = .6,
+                 color = ~ifelse(input$markers == "Tests", "#FFDD00", "#FF0000"), 
+                 stroke = F,
+                 group = "marker",
+                 label = ~ifelse(State == '', as.character(County), Location),
+                 popup = ~ifelse(State == '', as.character(County), Location),
+      )
+  })
+  
+  observeEvent(input$counties, {
+    groupToShow = "population"
+    
+    if (input$counties == "Beds") {
+      groupToShow = "beds"
+    }
+    if (input$counties == "Elderly Population") {
+      groupToShow = "elderly"
+    }
+    if (input$counties == "Comorbidities") {
+      groupToShow = "comorbidities"
+    }
+    leafletProxy("map", data = aggregated) %>%
+      hideGroup("population") %>%
+      hideGroup("beds") %>%
+      hideGroup("elderly") %>%
+      hideGroup("comorbidities") %>%
+      showGroup(groupToShow)
   })
   
   observeEvent(input$map_shape_click, {
-    id <- strsplit(input$map_shape_click$id, ",")[[1]]
+    print("click")
+    if (input$map_shape_click$group != "marker") {
+      return()
+    }
+    
+    id <- strsplit(input$map_shape_click$id, ", ")[[1]]
     output$logisticCurve <- renderPlot({
       subset <- obs[County == id[1] & State == id[2] & Positive]
       setorder(subset, Updated)
